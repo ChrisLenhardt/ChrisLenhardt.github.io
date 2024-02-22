c992b43b091046f181b9410122421a27ae371db2
notSpainChris <christopher.e.lenhardt@gmail.com>
2024-02-22 01:02:19 -0500

    Adjusted sizing and scaling, simplified nesting

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 const interval = setInterval(function() {
     var scrollTop = $(window).scrollTop();
-    console.log(scrollTop);
   }, 5000);
 
 
 addEventListener("scroll", (event) => {
     var scrollTop = $(window).scrollTop();
-    if (scrollTop > 600) {
+    if (scrollTop > 600 && scrollTop < 1500) {
         var elements = document.getElementsByClassName("myNavBar");
-        console.log(elements[0])
         for (i = 0, len = elements.length;  i < len; i++) {
             elements[i].style.color = 'white';
-            console.log("white")
+        }
+    } else if (scrollTop > 1200) {
+        var elements = document.getElementsByClassName("myNavBar");
+        for (i = 0, len = elements.length;  i < len; i++) {
+            elements[i].style.color = 'rgb(25,25,25)';
         }
     } else {
         var elements = document.getElementsByClassName("myNavBar");
