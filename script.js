30f63ae96b814217bb69aece75cf65b12c38511a
notSpainChris <christopher.e.lenhardt@gmail.com>
2024-02-22 01:35:11 -0500

    Created a card of experience, removed snap scroll, filled about me

--- script.js
+++ script.js
@@ -5,18 +5,13 @@ const interval = setInterval(function() {
 
 addEventListener("scroll", (event) => {
     var scrollTop = $(window).scrollTop();
-    if (scrollTop > 600 && scrollTop < 1500) {
-        var elements = document.getElementsByClassName("myNavBar");
+    if (scrollTop > 900) {
+        var elements = document.getElementsByClassName("nav-link");
         for (i = 0, len = elements.length;  i < len; i++) {
             elements[i].style.color = 'white';
         }
-    } else if (scrollTop > 1200) {
-        var elements = document.getElementsByClassName("myNavBar");
-        for (i = 0, len = elements.length;  i < len; i++) {
-            elements[i].style.color = 'rgb(25,25,25)';
-        }
     } else {
-        var elements = document.getElementsByClassName("myNavBar");
+        var elements = document.getElementsByClassName("nav-link");
         for (i = 0, len = elements.length;  i < len; i++) {
             elements[i].style.color = 'rgb(25,25,25)';
         }
